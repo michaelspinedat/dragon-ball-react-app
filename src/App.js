d2921de889ecdf285ae09538b9c686d94d9d8bad
Michael Steven Pineda Torres <stevenpnd@gmail.com>
2022-02-13 17:52:26 -0500

    Set up context and reducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect, useReducer } from 'react'
+import { AuthContext } from './contexts/AuthContext';
+import { AuthReducer } from './reducers/AuthReducer'
 import AppRouter from './routes/AppRouter'
 
 const App = () => {
+
+    const init = () => JSON.parse(localStorage.getItem("log")) || { log: false };
+
+    const [log, dispatch] = useReducer(AuthReducer, {}, init);
+
+    useEffect(() => {
+        localStorage.setItem("log", JSON.stringify(log));
+    }, [log]);
+
+
     return (
-        <div className="container">        
-            <AppRouter />
+        <div className="container">
+            <AuthContext.Provider value={{ log, dispatch }}>
+                <AppRouter />
+            </AuthContext.Provider>
         </div>
     )
 }
